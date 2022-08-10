3b3b1367dfd58e6e209879a9ac2bfb1ea0402760
kzmijak@gmail.com <kzmijak@gmail.com>
2022-08-10 19:48:19 +0200

    change layout ratio

--- src/_zswod/layouts/dashboard/src/components/DashboardLayout.tsx
+++ src/_zswod/layouts/dashboard/src/components/DashboardLayout.tsx
@@ -20,14 +20,12 @@ type MainStyleProps = {
 const MainStyle = styled('main', {
   shouldForwardProp: (prop) => prop !== 'collapseClick',
 })<MainStyleProps>(({ collapseClick, theme }) => ({
-  flexGrow: 1,
-  paddingTop: HEADER.MOBILE_HEIGHT + 24,
-  paddingBottom: HEADER.MOBILE_HEIGHT + 24,
+  paddingBottom: 24,
   [theme.breakpoints.up('lg')]: {
     paddingLeft: 16,
     paddingRight: 16,
-    paddingTop: HEADER.DASHBOARD_DESKTOP_HEIGHT + 24,
-    paddingBottom: HEADER.DASHBOARD_DESKTOP_HEIGHT + 24,
+    paddingTop: 24,
+    paddingBottom: 24,
     width: `calc(100% - ${NAVBAR.DASHBOARD_WIDTH}px)`,
     transition: theme.transitions.create('margin-left', {
       duration: theme.transitions.duration.shorter,
