8b3cca499bd4b2094c2de267cad7857b49a75c7c
kzmijak@gmail.com <kzmijak@gmail.com>
2023-03-14 19:56:21 +0100

    register gallery model

--- src/_zswod/modules/CurrentGallery/src/slice/reducer.ts
+++ src/_zswod/modules/CurrentGallery/src/slice/reducer.ts
@@ -1,14 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { GalleryModel } from 'src/_zswod/models/Gallery';
 import { RequestStatus } from 'src/_zswod/utils/requestStatus';
 
 type CurrentGalleryState = {
   status: RequestStatus;
   error: string;
+  gallery: GalleryModel | undefined;
 };
 
 const initialState: CurrentGalleryState = {
   status: 'idle',
   error: '',
+  gallery: undefined,
 };
 
 const slice = createSlice({
